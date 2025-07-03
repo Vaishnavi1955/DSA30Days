c023b4f6ec7d3c9654b08cb5f08ca7109f95e8e1
Vaishnavi1955 <phadvaishnavi1905@gmail.com>
2025-07-03 09:05:49 +0530

    IntersectionofTwoArrays

---
+++ IntersectionofTwoArrays.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+
+        int i = 0, j = 0;
+        vector<int> ans;
+
+        while (i < nums1.size() && j < nums2.size()) {
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else if (nums2[j] < nums1[i]) {
+                j++;
+            } else {
+                if (ans.empty() || ans.back() != nums1[i]) {
+                    ans.push_back(nums1[i]);
+                }
+                i++;
+                j++;
+            }
+        }
+        return ans;
+    }
+};
+
+int main() {
+    vector<int> nums1 = {1, 2, 2, 1};
+    vector<int> nums2 = {2, 2};
+
+    Solution sol;
+    vector<int> result = sol.intersection(nums1, nums2);
+
+    cout << "Intersection: ";
+    for (int num : result) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
+
